7cb6ddebfd89a886e5a2c174b46c68fa9e7f9471
teddyirwn <tdyirawan0@gmail.com>
2026-05-13 11:44:42 +0700

    fix size

--- src/class/Fighter.js
+++ src/class/Fighter.js
@@ -72,6 +72,7 @@ export class Fighter extends Sprite {
   attack() {
     this.switchSprite("attack");
     this.isAttacking = true;
+
     setTimeout(() => {
       this.isAttacking = false;
     }, 100);
--- src/data/config.js
+++ src/data/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   gravity: 0.3,
-  canvasWidth: 800,
-  canvasHeight: 450,
-  groundMargin: 200,
+  canvasWidth: 1366,
+  canvasHeight: 768,
+  groundMargin: 300,
 };
